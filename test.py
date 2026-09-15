215acf82454fb8957337557f716e6e36f9e8952b
.zero <elmercalambro94@gmail>
2026-09-14 20:41:26 -0700

    first commit

--- test.py
+++ test.py
@@ -25,9 +25,21 @@ class Square(Shape):
         return f"Square(area={self.area():.1f})"
 
 
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def __str__(self):
+        return f"Rectangle(area={self.area():.1f})"
+
+
 def print_area(shape):
     print(shape, "-> area:", shape.area())
 
 
-for s in [Circle(3), Square(4)]:
+for s in [Circle(3), Square(4), Rectangle(length=5, width=2)]:
     print_area(s)
